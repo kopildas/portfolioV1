1e594d83e21a6d0c1bcb67869125e333ea33bf64
kopildas <102140363+kopildas@users.noreply.github.com>
2023-12-12 13:04:28 +0600

    feat: image box custom carousels

--- client/src/cpmponenets/Home/Pro_Box_1/Pro_Box_1.jsx
+++ client/src/cpmponenets/Home/Pro_Box_1/Pro_Box_1.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { IoMdArrowDropright,IoMdArrowDropleft,IoMdArrowDropleftCircle,IoMdArrowDroprightCircle } from "react-icons/io";
 
-export default function Pro_Box_1({id,data}) {
-    console.log(id)
+export default function Pro_Box_1({project_number,data}) {
+
+    const[number,setNumber] = useState(1)
+    const[displayImage,setDisplayImage] = useState(data[0].image)
+
+
+    const decrease = ()=> {
+        if(number <= 1)
+        {
+            setNumber(data.length)
+        }else {
+            setNumber(number - 1)
+        }
+    }
+    const increase = ()=> {
+        if(number >= data.length)
+        {
+            setNumber(1)
+        }else {
+            setNumber(number + 1)
+        }
+    }
+
+    useEffect(() => {
+      setDisplayImage(data[number-1].image) // because number started from 1 and id also started from 1 so we have to decrease it by 1
+    }, [number])
+
+
   return (
     <div className='w-3/5 h-auto hover:bg-gradient-1 ease-in-out duration-500 rounded-3xl'>
-            <div className=' h-auto rounded-3xl flex flex-col border border-shades-4 backdrop-blur-lg ' style={{
+            <div className=' h-auto rounded-3xl flex flex-col border border-shades-4 backdrop-blur-xl back ' style={{
               backgroundColor: "#0f3a3322",
             }} >
                 <div className=' backdrop-blur-md rounded-3xl'>
@@ -15,8 +42,17 @@ export default function Pro_Box_1({id,data}) {
                         </div>
                         <div>arrow</div>
                     </div>
-                    <div className='o object-contain p-5'>
-                    <img src="https://i.ibb.co/c20Tz5D/mobile.png" alt="mobile" border="0" className='w-full h-96 rounded-3xl'/>
+                    <div className='o object-contain p-5 relative flex items-center justify-center'>
+                    <img src={displayImage} alt="mobile" border="0" className='w-full rounded-3xl'/>
+                    
+                    <div className='w-full  h-10 bg-red-40 absolute flex items-center justify-between p-5'>
+                        <div className=' w-10 h-10 flex bg-orange-200 opacity-60 hover:opacity-100 hover:text-7xl hover:w-12 hover:h-12 ease-in-out duration-300 items-center justify-center text-5xl cursor-pointer rounded-full' onClick={decrease}>
+                            <IoMdArrowDropleft/>
+                        </div>
+                        <div className=' w-10 h-10 bg-orange-200 opacity-60 hover:opacity-100 hover:text-7xl hover:w-12 hover:h-12 ease-in-out duration-300 cursor-pointer rounded-full flex items-center justify-center text-5xl' onClick={increase}>
+                        <IoMdArrowDropright />
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
--- client/src/cpmponenets/Home/Project_1/Project_1.jsx
+++ client/src/cpmponenets/Home/Project_1/Project_1.jsx
@@ -4,7 +4,15 @@ import Pro_Box_1 from '../Pro_Box_1/Pro_Box_1'
 export default function Project_1() {
 
     const one=1;
-
+    const data = [
+        { id: 1, image: "https://i.ibb.co/c20Tz5D/mobile.png" },
+        { id: 2, image: "https://i.ibb.co/7zGf3LR/mobile-2.png" },
+        { id: 3, image: "https://i.ibb.co/bd6HqKS/mobile-3.png" },
+        { id: 4, image: "https://i.ibb.co/7tXdLrt/mobile-4.png" },
+        { id: 5, image: "https://i.ibb.co/mD4mX4Y/mobile-5.png" },
+        { id: 6, image: "https://i.ibb.co/kDzGWhX/mobile-6.png" },
+        { id: 7, image: "https://i.ibb.co/svXb43g/mobile-7.png" },
+    ];
 
   return (
     <section className='h-auto flex  bg-shades-1 '>
@@ -15,7 +23,7 @@ export default function Project_1() {
         </div>
 
         <div className='flex items-center justify-center mt-10 font2 rounded-3xl gap-5'>
-            <Pro_Box_1 id={one}/>
+            <Pro_Box_1 project_number={one} data={data}/>
             <div className='w-2/5 h-full flex items-end justify-end'>
                 <div className='w-full h-auto bg-shades-7 border border-shades-8 rounded-3xl'>
                     <div className='w-full h-auto p-5 rounded-3xl border border-shades-5 text-stone-400'>
