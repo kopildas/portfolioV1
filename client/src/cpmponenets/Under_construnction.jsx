676313cb711a4becde0702ae60de536d7e2935fa
kopildas <102140363+kopildas@users.noreply.github.com>
2023-12-28 00:56:25 +0600

    fix: responsiveness

--- client/src/cpmponenets/Under_construnction.jsx
+++ client/src/cpmponenets/Under_construnction.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Under_construnction() {
   return (
     <div className='flex flex-col items-center justify-center h-screen text-gray-500 p-20 bg-shades-2'>
-        <p className='text-6xl mb-5 -mt-5'>This page currently</p>
-        <p className='text-6xl mb-5 -mt-5'>under construction...</p>
+        <p className='text-2xl lg:text-6xl mb-5 -mt-5'>This page currently</p>
+        <p className='text-2xl lg:text-6xl mb-5 -mt-5'>under construction...</p>
       <div>
-      <iframe src="https://giphy.com/embed/f7b9ltJ4FrhnsKjYx2" width="480" height="270" frameBorder="0" className="giphy-embed rounded-xl" allowFullScreen></iframe>
+      <iframe src="https://giphy.com/embed/f7b9ltJ4FrhnsKjYx2" width="480" height="270" frameBorder="0" className="giphy-embed rounded-xl p-8 lg:p-0" allowFullScreen></iframe>
       </div>
     </div>
   )
