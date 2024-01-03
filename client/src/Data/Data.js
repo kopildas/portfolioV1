a7dfc80a0030265bf0107b09db0b3840a0def43a
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-04 01:02:15 +0600

    feat: projects page

---
+++ client/src/Data/Data.js
@@ -0,0 +1,150 @@
+export const data = [
+    {
+        "category" : "MERN", 
+        "image": "https://i.ibb.co/yRzNddw/mobile-27.png",
+        "name": "Whoody + Admin panel",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+            "f3": "fsdfsd sdgs  sgsdg",
+            "f4": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Javascript",
+            "t2": "React",
+            "t3": "Tailwind css",
+            "t4": "Node.js",
+            "t5": "Express.js",
+            "t6": "MongoDB",
+        },"github" : "https://github.com/kopildas/furniture",
+        "live" : "https://woodhy.vercel.app/"
+    },
+    {
+        "category" : "MERN", 
+        "image": "https://i.ibb.co/8cj4Dn1/mobile-26.png",
+        "name": "Omano v2",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Javascript",
+            "t2": "React",
+            "t3": "Tailwind css",
+            "t4": "JWT",
+            "t5": "Node.js",
+            "t6": "Express.js",
+            "t7": "MongoDB",
+        },
+        "github" : "https://github.com/kopildas/omano",
+        "live" : "https://omano.vercel.app/"
+    },
+    {
+        "category" : "MERN", 
+        "image": "https://i.ibb.co/R9cNsRn/mobile-28.png",
+        "name": "Omano v1 + Admin panel",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Javascript",
+            "t2": "React",
+            "t3": "Tailwind css",
+            "t4": "Node.js",
+            "t5": "Express.js",
+            "t6": "Firebase",
+            "t7": "Firebase Auth",
+        },
+        "github" : "https://github.com/kopildas/omano",
+        "live" : "https://omano-restaurant.vercel.app/"
+    },
+  
+    {
+        "category" : "React", 
+        "image": "https://i.ibb.co/yhDGdC0/mobile-29.png",
+        "name": "Karfolio",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+            "f3": "fsdfsd sdgs  sgsdg",
+            "f4": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Javascript",
+            "t2": "React",
+            "t3": "Tailwind css",
+            "t4": "Framer motion",
+        },
+        "github" : "https://github.com/kopildas/karprotfolio",
+        "live" : "https://karfolio.onrender.com/"
+    },
+    {
+        "category" : "HTML", 
+        "image": "https://i.ibb.co/ZgRL64T/mobile-30.png",
+        "name": "Client's portfolio",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+            "f3": "fsdfsd sdgs  sgsdg",
+            "f4": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Javascript",
+            "t2": "HTML",
+            "t3": "CSS",
+        },"github" : "https://github.com/kopildas/pappuportfolio",
+        "live" : "https://pappukar.com/"
+    },
+    {
+        "category" : "Java", 
+        "image": "https://i.ibb.co/tmTyTWL/HiRes-17.jpg",
+        "name": "2d ping-pong game",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+            "f3": "fsdfsd sdgs  sgsdg",
+            "f4": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "Java",
+            "t2": "Java Swing",
+            "t3": "Websoket",
+            
+        },"github" : "https://github.com/kopildas/2d-ping-pong-game-java",
+        "live" : ""
+    },
+    {
+        "category" : "ML", 
+        "image": "https://i.ibb.co/tmTyTWL/HiRes-17.jpg",
+        "name": "Multi Disease Prediction System",
+        "description": "asfak sjhfj kas hdfas hdfkh ahdfad asdfh",
+        "features":
+        {
+            "f1": "fsdfsd sdgs  sgsdg",
+            "f2": "oo pofg ddfsd sdgs  sgsdg",
+            "f3": "fsdfsd sdgs  sgsdg",
+            "f4": "oo pofg ddfsd sdgs  sgsdg",
+        },
+        "tech": {
+            "t1": "CNN",
+            "t2": "SVM",
+            "t3": "Transfer Learning ",
+            "t4": "Naive Bais",
+        },"github" : "https://github.com/TeamMalloc/Multi-Disease-Prediction-System",
+        "live" : ""
+    },
+    
+]
--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
-import Under_construnction from '../../cpmponenets/Under_construnction'
+import React, { useEffect, useState } from "react";
+import Under_construnction from "../../cpmponenets/Under_construnction";
+import { data } from "../../Data/Data";
+import Project_card from "../../cpmponenets/Project_card/Project_card";
 
 export default function Projects() {
+  const [projects,setProjects] = useState(data)
+  const category = [
+    { value: "All" },
+    {
+      value: "MERN",
+    },
+    {
+      value: "React",
+    },
+    { value: "HTML" },
+    { value: "Java" },
+    { value: "ML" },
+  ];
+  const [filter, setFilter] = useState("All");
+  useEffect(() => {
+    if (filter !== "All") {
+      setProjects(data?.filter((n) => n.category === filter));
+    }
+    else{
+      setProjects(data)
+    }
+  }, [data, filter]);
+  console.log(data);
+  console.log(projects)
   return (
-    <div>
-      <Under_construnction/>
+    <div className="h-auto flex flex-col bg-shades-1">
+      <div className="flex mt-16 p-10 flex-col">
+        <p className="text-xl lg:text-7xl font1  text-shades-8">Projects</p>
+        <div className="lg:w-[15rem] h-[4rem] -mt-[29px] -ml-5 border-2 border-shades-4"></div>
+      </div>
+
+      <div className="w-full h- bg-shades-1 flex items-center justify-center">
+        <div className="w- flex gap-5 font1">
+        {category &&
+                      category.map((category) => (
+                        <div
+                          key={category?.id}
+                          onClick={() => setFilter(category.value)}
+                        >
+                          <div
+                            className={`group ${
+                              filter === category.value
+                                ? "bg-orange-300"
+                                : "bg-slate-100 hover:bg-orange-100"
+                            } w-20 h-8  cursor-pointer rounded-lg drop-shadow-xl flex flex-row md:flex-col gap-3 items-center justify-center duration-200 transition-all ease-in-out `}
+                          >
+                            <p className="text-md font-semibold">
+                               {category.value}
+                            </p>
+                          </div>
+                        </div>
+                      ))}
+        </div>
+        
+      </div>
+
+      <div className="w-full h-auto bg-shades-1 flex items-center justify-center ">
+        {projects && <Project_card projects={projects}/>}
+</div>
     </div>
-  )
+  );
 }
