20b21c291e341eb0805126cd21fa20414f2f8581
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-26 18:56:02 +0600

    feat: education added

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -11,14 +11,32 @@ export default function About() {
           className="w-80"
         ></img>
       </div>
-      <div className="w-[60%] h-full bg-shades-2  flex flex-col items-start justify-center p-10">
-        <div>
-          <p className="text-4xl text-font">I'm a frontend developer.</p>
-          <p className="text-xl mt-3 font3">
-            Recent graduate in Computer Science and Engineering from <b> North East University Bangladesh</b>. Currently I'm working with <b>MERN</b> stack React.js, Node.js, Express.js,
-            & MongoDB. <br /> Every time I want to develop myself by learning
-            new skills that helps me to build a strong career.
+      <div className="w-[60%] overflow-y-auto no-scrollbar h-full bg-shades-2  flex flex-col items-start justify-center p-10">
+        <div className="mt-[10rem]">
+          <p className="text-4xl text-font text-shades-1">
+            I'm a frontend developer.
           </p>
+          <p className="text-xl mt-3 font3 text-shades-7">
+            Recent graduate in 
+            <b> Computer Science and Engineering</b> from North East University Bangladesh. Currently I'm working with{" "}
+            <b>MERN</b> stack React.js, Node.js, Express.js, & MongoDB. <br />{" "}
+            Every time I want to develop myself by learning new skills that
+            helps me to build a strong career.
+          </p>
+        </div>
+
+        <div className="mt-10 font3 text-shades text-black">
+          <p className="text-2xl text-font text-zinc-950">Education</p>
+          <p>Sept 2019 - Jan 2024</p>
+          <p>North East University Bangladesh</p>
+          <p>
+            Bachelor of Science with Honours in Computer Science and Engineering
+          </p>
+        </div>
+        <div className="mt-5 font3 text-black">
+          <p>Apr 2015 - May 2017</p>
+          <p>Modon Muhan College Sylhet</p>
+          <p>HSC(Science) </p>
         </div>
       </div>
     </div>
