251db1db708ba13bb34075ab3d897d06f29c9f37
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-26 17:57:08 +0600

    feat: basic structure of about page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import Under_construnction from '../../cpmponenets/Under_construnction'
+import React from "react";
+import Under_construnction from "../../cpmponenets/Under_construnction";
 
 export default function About() {
   return (
-    <div>
-            <Under_construnction/>
+    <div className="h h-screen bg-shades-1 w-full flex">
+      <div className="w-[40%] h-full flex items-center justify-center">
+        <img
+          src="https://i.ibb.co/vDh9vtt/Profile-picture-Photo-Room-removebg-preview.png"
+          border="0"
+          className="w-80"
+        ></img>
+      </div>
+      <div className="w-[60%] h-full bg-shades-2  flex flex-col items-start justify-center p-10">
+        <div>
+          <p className="text-4xl text-font">I'm a frontend developer.</p>
+          <p className="text-xl mt-3 font3">
+            Recent graduate in Computer Science and Engineering from <b> North East University Bangladesh</b>. Currently I'm working with <b>MERN</b> stack React.js, Node.js, Express.js,
+            & MongoDB. <br /> Every time I want to develop myself by learning
+            new skills that helps me to build a strong career.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
