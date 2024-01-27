3a2405dce5f73d727a4f1f5871bf209b2a41e1ae
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-27 22:41:34 +0600

    feat: machine learning project added

--- client/src/cpmponenets/Home/Project_4/Project_4.jsx
+++ client/src/cpmponenets/Home/Project_4/Project_4.jsx
@@ -1,21 +1,49 @@
 import React from "react";
-import "./Project_1.css";
+import "../Project_1/Project_1.css";
 import Pro_Box_1 from "../Pro_Box_1/Pro_Box_1";
-export default function Project_1() {
+
+export default function Project_4() {
   const one = 1;
   const data = [
-    { id: 1, image: "https://i.ibb.co/c20Tz5D/mobile.png" },
-    { id: 2, image: "https://i.ibb.co/7zGf3LR/mobile-2.png" },
-    { id: 3, image: "https://i.ibb.co/bd6HqKS/mobile-3.png" },
-    { id: 4, image: "https://i.ibb.co/7tXdLrt/mobile-4.png" },
-    { id: 5, image: "https://i.ibb.co/mD4mX4Y/mobile-5.png" },
-    { id: 6, image: "https://i.ibb.co/kDzGWhX/mobile-6.png" },
-    { id: 7, image: "https://i.ibb.co/svXb43g/mobile-7.png" },
+    {
+      id: 1,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 2,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 3,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 4,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 5,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 6,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
+    {
+      id: 7,
+      image: "https://i.ibb.co/rbcr3Dy/Screenshot-2024-01-27-221813.png",
+    },
   ];
 
   return (
     <section className="h-auto flex  bg-shades-1 z-20 ">
       <div className="lg:p-20 p-5 flex flex-col w-full">
+        <div className="flex flex-col items-end justify-end">
+          <p className="text-2xl lg:text-8xl font1 z-10 text-shades-8">
+            Projects
+          </p>
+          <div className="lg:w-[18rem] h-[4rem] -mt-[38px] -mr-7 border-2 border-shades-4"></div>
+        </div>
 
         <div className="lg:px-10 bg-red-00 flex flex-col lg:flex-row items-center justify-center mt-10 mb-10 font2 rounded-3xl gap-5 border border-shades-3 p-[5px]">
           <div className="lg:w-3/5 h-auto hover:bg-gradient-1 ease-in-out duration-500 rounded-3xl">
@@ -28,9 +56,9 @@ export default function Project_1() {
               <div className=" backdrop-blur-md rounded-3xl">
                 <div className="flex items-center justify-between lg:p-10 p-5 border m-1 border-shades-6 rounded-3xl">
                   <div className="flex flex-col">
-                    <p className="text-2xl text-white">Woodhy</p>
+                    <p className="text-2xl text-white">Medvision-Ai</p>
                     <p className="text-white text-xl text-font">
-                      A MERN stack project
+                      A Machine Learning project
                     </p>
 
                     <div className="flex mt-2 text-sm opacity-80">
@@ -52,43 +80,66 @@ export default function Project_1() {
           <div className="lg:w-2/5 h-full flex items-end justify-end">
             <div className="w-full h-auto bg-shades-7 border border-shades-8 rounded-3xl">
               <div className="w-full h-auto p-5 rounded-3xl border border-shades-5 text-stone-400 text-font text-xl">
-                <p>Woodhy, is my latest MERN Stack creation!</p>
+                <p>
+                  Medical Image Recognition Web App utilizing Machine Learning
+                </p>
                 <br />
                 <p>Features:</p>
                 <p className="t text-base 2xl:text-lg">
                   <ul>
-                    <li>◼ User-friendly single page application</li>
-                    <li>◼ Sleek admin panel for efficient management</li>
-                    <li>◼ Dynamic sorting for an enhanced user experience</li>
-                    <li>◼ Features dynamic sorting, customizable pricing options, and seamless review addition </li>
-                    <li>◼ JWT ensuring secure user authentication</li>
-                    <li>◼ Innovative "favorite" button for personalized interaction
-</li>
+                    <li>
+                      ◼ Developed a Streamlit web app for medical image analysis.
+                    </li>
+                    <li>
+                      ◼ Tested and validated models on diverse datasets.
+                    </li>
+                    <li>
+                      ◼ Achieved 72% accuracy in Naive Bayes and CNN attained
+                      87% accuracy in Chest X-ray classification.
+                    </li>
+                    <li>
+                      ◼ Collaboratively developed Transfer Learning for enhanced
+                      accuracy.
+                    </li>
                   </ul>
                 </p>
                 <br />
                 <p>Technology Stack:</p>
                 <div className="flex flex-wrap gap-2 text-base mt-2">
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">React</p>
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">Tailwind CSS</p>
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">MongoDB</p>
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">Mongoose ODM</p>
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">Node.js</p>
-                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">Express.js</p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                  Python
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                    OpenCV
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                  Streamlit
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                  TensorFlow
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                    CNN
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                    Transfer Learning
+                  </p>
+                  <p className="b bg-shades-1 text-shades-8 p-2 px-4 rounded-xl">
+                    Naive Bayes
+                  </p>
                 </div>
-
               </div>
               <div className="w-full h-auto rounded-3xl border border-shades-5 text-stone-400 flex items-center justify-center">
                 <a
                   className="lg:w-1/2 rounded-3xl bg-shades-8 h-full flex items-center justify-center text-shades-1 p-5 border border-shades-7 text-xl"
-                  href="https://github.com/kopildas/furniture"
+                  href="https://github.com/TeamMalloc/final-year-ML-projcet"
                 >
                   <div>Github link</div>
                 </a>
                 <div></div>
                 <a
                   className="lg:w-1/2 rounded-3xl bg-shades-8 h-full flex items-center justify-center text-shades-1 p-5 border border-shades-7 text-xl"
-                  href="https://woodhy.vercel.app/"
+                  href="https://huggingface.co/spaces/kopildas/medvision-ai"
                 >
                   <div>Go to the website</div>
                 </a>
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,12 +4,14 @@ import Skill from '../../cpmponenets/Home/Skill/Skill';
 import Project_1 from '../../cpmponenets/Home/Project_1/Project_1';
 import Project_2 from '../../cpmponenets/Home/Project_2/Project_2';
 import Project_3 from '../../cpmponenets/Home/Project_3/Project_3';
+import Project_4 from '../../cpmponenets/Home/Project_4/Project_4';
 import Skills from '../../cpmponenets/Home/Skill/Skills';
 import TextReveal from '../../cpmponenets/Home/Skill/TextReveal';
 import TextReveal_2 from '../../cpmponenets/Home/Skill/TextReveal_2';
-
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
   const [offsetY, setOffsetY] = useState(0);
 
   const handleScroll = () => setOffsetY(window.scrollY);
@@ -32,9 +34,15 @@ export default function Home() {
       {/* <TextReveal/>
       <TextReveal_2/> */}
       {/* <Skill /> */}
+      <Project_4 />
       <Project_1 />
       <Project_2 />
-      <Project_3 />
+
+      <div className='w-full bg-red-00 flex items-center justify-center -mt-10 mb-20'>
+          <p className='px-7 py-2 bg-shades-4 text-xl font-semibold text-shades-1 rounded-2xl hover:scale-110 duration-200 transition ease-in-out cursor-pointer' onClick={() => {
+            navigate("/projects");
+          }}>VIew More</p>
+        </div>
     </div>
   );
 }
