a9fb96ad3d9e0dbb94a96c9403a83038e7b8f01c
kopildas <102140363+kopildas@users.noreply.github.com>
2024-01-27 21:45:02 +0600

    fix: responsiveness feat: socials add

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Under_construnction from "../../cpmponenets/Under_construnction";
+import Icon_Magnetic from "../../cpmponenets/Home/Hero/Icon_Magnetic";
+import { FaLinkedinIn, FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
 export default function About() {
   return (
-    <div className="h h-screen bg-shades-1 w-full flex">
-      <div className="w-[40%] h-full flex items-center justify-center">
+    <div className="h h-screen bg-shades-1 w-full flex flex-col md:flex-row">
+      <div className="w-[40%] h-full flex items-center justify-center mt-20 md:mt-0 mx-3 my-3 md:mx-0 md:my-0">
         <img
           src="https://i.ibb.co/vDh9vtt/Profile-picture-Photo-Room-removebg-preview.png"
           border="0"
           className="w-80"
         ></img>
       </div>
-      <div className="w-[60%] overflow-y-auto no-scrollbar h-full bg-shades-2  flex flex-col items-start justify-center p-10">
-        <div className="mt-[10rem]">
+      <div className="md:w-[60%] md:overflow-y-auto no-scrollbar md:h-full bg-shades-2  flex flex-col items-start justify-center p-10">
+        <div className="md:mt-[10rem]">
           <p className="text-4xl text-font text-shades-1">
             I'm a frontend developer.
           </p>
@@ -38,6 +40,38 @@ export default function About() {
           <p>Modon Muhan College Sylhet</p>
           <p>HSC(Science) </p>
         </div>
+
+
+
+        <p className="text-2xl text-font text-zinc-950 mt-10">Socials</p>
+        <div className="flex w-40 items-center h-auto  gap-5 mt-1 text-shades-4 z-10">
+       
+          
+       <div className='' >
+       <Icon_Magnetic>
+       <a href="https://www.linkedin.com/in/kopildas/" className='cursor-pointer text-2xl' ><FaLinkedinIn /></a>
+           
+         
+       </Icon_Magnetic>
+       </div>
+       <Icon_Magnetic>
+       <a href="https://github.com/kopildas" className='cursor-pointer text-2xl'><FaGithub /></a>
+           
+         
+       </Icon_Magnetic>
+       <Icon_Magnetic>
+       <a href="https://twitter.com/KopilDas_" className='cursor-pointer text-2xl'><FaTwitter /></a>
+           
+         
+       </Icon_Magnetic>
+       <Icon_Magnetic>
+       <a href="https://www.instagram.com/kopildas_/" className='cursor-pointer text-2xl'> <FaInstagram /></a>
+          
+         
+       </Icon_Magnetic>
+     </div>
+
+
       </div>
     </div>
   );
