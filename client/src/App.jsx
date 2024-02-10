04e58afbac4d3cc6d664eb0606b3c0e7b6a866f6
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-11 00:44:49 +0600

    feat: @vercel/analytics added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // import Header from './component/Header/Header'
-import { BrowserRouter as Router,
+import {
+  BrowserRouter as Router,
   Routes,
   Route,
-  useLocation, } from 'react-router-dom'
+  useLocation,
+} from "react-router-dom";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Header from './cpmponenets/Header/Header'
-import Lenis from '@studio-freight/lenis'
-import Projects from './pages/Projects/Projects'
-import Contact from './pages/Contact/Contact'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Header from "./cpmponenets/Header/Header";
+import Lenis from "@studio-freight/lenis";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   // this is for smooth scrolling by lenis
 
-  
-
   return (
     <>
-    
-       <div className="w-screen h-auto flex flex-col  bg-shades-1">
-      <Router>
-        {/* <Cursor scaling={scaling}/> */}
-        <Header />
-        <Routes>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-
-        </Routes>
-      </Router>
+      <div className="w-screen h-auto flex flex-col  bg-shades-1">
+        <Router>
+          {/* <Cursor scaling={scaling}/> */}
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Analytics />
+        </Router>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
